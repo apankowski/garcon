98209b272e674cd853ca4020c5ebcbacdec5885a
Andrzej Pańkowski <github@irratio.nl>
2021-04-29 14:35:30 +0200

    Groups fixtures

--- src/test/kotlin/dev/pankowski/garcon/domain/Fixtures.kt
+++ src/test/kotlin/dev/pankowski/garcon/domain/Fixtures.kt
@@ -5,11 +5,29 @@ import java.time.Duration
 import java.time.Instant
 import java.util.*
 
+// Configs
+fun someLunchConfig(
+  slackWebhookUrl: URL = URL("https://slack/webhook"),
+  syncInterval: Duration? = Duration.ofMinutes(5),
+  pages: List<LunchPageConfig> = emptyList()
+) = LunchConfig(slackWebhookUrl, syncInterval, pages,)
+
+fun someClientConfig(
+  userAgent: String = "Some User Agent",
+  timeout: Duration = Duration.ofSeconds(5)
+) = LunchClientConfig(userAgent, timeout)
+
+fun somePageConfig(
+  pageId: PageId = PageId("PID1"),
+  url: URL = URL("http://localhost:4321/posts")
+) = LunchPageConfig(pageId, url)
+
 fun somePostConfig(
   locale: Locale = Locale("pl", "PL"),
   keywords: List<Keyword> = listOf(Keyword("lunch", 1), Keyword("lunchowa", 2)),
 ) = LunchPostConfig(locale, keywords)
 
+// Domain
 fun somePost(
   externalId: ExternalId = ExternalId("FBID1"),
   link: URL = URL("https://facebook/post"),
@@ -18,19 +36,3 @@ fun somePost(
 ) = Post(externalId, link, publishedAt, content)
 
 fun somePageName(name: String = "some name") = PageName(name)
-
-fun somePageConfig(
-  pageId: PageId = PageId("PID1"),
-  url: URL = URL("http://localhost:4321/posts")
-) = LunchPageConfig(pageId, url)
-
-fun someClientConfig(
-  userAgent: String = "Some User Agent",
-  timeout: Duration = Duration.ofSeconds(5)
-) = LunchClientConfig(userAgent, timeout)
-
-fun someLunchConfig(
-  slackWebhookUrl: URL = URL("https://slack/webhook"),
-  syncInterval: Duration? = Duration.ofMinutes(5),
-  pages: List<LunchPageConfig> = emptyList()
-) = LunchConfig(slackWebhookUrl, syncInterval, pages,)
