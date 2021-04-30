f5e64b023e71f498656140d6844b662ed7d32ce1
Andrzej Pańkowski <github@irratio.nl>
2021-04-30 22:06:13 +0200

    Migrates tests of rest template slack reposter to Kotlin

---
+++ src/test/kotlin/dev/pankowski/garcon/infrastructure/RestTemplateSlackReposterTest.kt
@@ -0,0 +1,54 @@
+package dev.pankowski.garcon.infrastructure
+
+import dev.pankowski.garcon.domain.PageName
+import dev.pankowski.garcon.domain.someLunchConfig
+import dev.pankowski.garcon.domain.somePost
+import io.kotest.core.spec.style.FreeSpec
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.test.web.client.MockRestServiceServer
+import org.springframework.test.web.client.match.MockRestRequestMatchers.*
+import org.springframework.test.web.client.response.MockRestResponseCreators.withStatus
+import java.net.URL
+
+class RestTemplateSlackReposterTest : FreeSpec({
+
+  "synchronized post is reposted via incoming webhook" {
+    // given
+    val post = somePost(
+      link = URL("https://www.facebook.com/post"),
+      content = "Some post content",
+    )
+
+    val pageName = PageName("Some page name")
+
+    val lunchConfig = someLunchConfig(
+      slackWebhookUrl = URL("https://slack/webhook")
+    )
+
+    val repostingClient = RestTemplateSlackReposter(lunchConfig, RestTemplateBuilder())
+    val mockServer = MockRestServiceServer.createServer(repostingClient.restTemplate)
+
+    mockServer.expect(requestTo(lunchConfig.slackWebhookUrl.toURI()))
+      .andExpect(method(HttpMethod.POST))
+      .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+      .andExpect(
+        content().json(
+          """
+          |{
+          |"text":"New <https://www.facebook.com/post|lunch post> from *Some page name* :tada:\n\n>>>Some post content"
+          |}
+          """.trimMargin()
+        )
+      )
+      .andRespond(withStatus(HttpStatus.OK))
+
+    // when
+    repostingClient.repost(post, pageName)
+
+    // then
+    mockServer.verify()
+  }
+})
