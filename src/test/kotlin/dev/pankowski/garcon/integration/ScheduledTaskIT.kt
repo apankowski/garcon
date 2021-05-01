6450460c8339f33f2f78859d44c94f0eec1652a3
Andrzej Pańkowski <github@irratio.nl>
2021-05-01 14:41:45 +0200

    Migrates scheduled task integration tests to Kotlin

---
+++ src/test/kotlin/dev/pankowski/garcon/integration/ScheduledTaskIT.kt
@@ -0,0 +1,45 @@
+package dev.pankowski.garcon.integration
+
+import dev.pankowski.garcon.domain.LunchService
+import io.kotest.assertions.timing.eventually
+import io.kotest.matchers.ints.beGreaterThan
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.test.context.TestPropertySource
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.time.ExperimentalTime
+import kotlin.time.seconds
+
+@TestPropertySource(
+  properties = [
+    "lunch.sync-interval: PT0.2S",
+    "spring.main.allow-bean-definition-overriding: true"
+  ]
+)
+@ExperimentalTime
+class ScheduledTaskIT(lunchService: LunchService) : CommonIT() {
+
+  // Maybe use https://github.com/Ninja-Squad/springmockk ?
+  @TestConfiguration
+  class Mocks {
+    @Bean
+    fun lunchService() = mockk<LunchService>()
+  }
+
+  init {
+    "checking for lunch post is called regularly" {
+      // given
+      val counter = AtomicInteger(0)
+
+      every { lunchService.synchronizeAll() } answers { counter.incrementAndGet() }
+
+      // expect
+      eventually(10.seconds) {
+        counter shouldBe beGreaterThan(2)
+      }
+    }
+  }
+}
