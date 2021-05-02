c8ba6a59151c4a5dd5f0a787deca8a9b9ad752cb
Andrzej Pańkowski <github@irratio.nl>
2021-05-02 17:34:37 +0200

    Cleans up version converter tests

--- src/test/kotlin/dev/pankowski/garcon/infrastructure/persistence/VersionConverterTest.kt
+++ src/test/kotlin/dev/pankowski/garcon/infrastructure/persistence/VersionConverterTest.kt
@@ -1,37 +1,33 @@
 package dev.pankowski.garcon.infrastructure.persistence
 
+import dev.pankowski.garcon.WithTestName
 import dev.pankowski.garcon.domain.Version
+import dev.pankowski.garcon.forAll
 import io.kotest.core.spec.style.FreeSpec
-import io.kotest.matchers.nulls.beNull
-import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
 
 class VersionConverterTest : FreeSpec({
 
-  val converter = VersionConverter()
-
-  "converts null to null and back" {
-    // expect
-    converter.from(null) should beNull()
-
-    // and
-    converter.to(null) should beNull()
+  data class TestCase(val versionNumber: Int?, val version: Version?) : WithTestName {
+    override fun testName() = "converts $versionNumber to $version and back"
   }
 
-  "converts from number to version and back" - {
-    listOf(
-      1 to Version(1),
-      8 to Version(8),
-      0 to Version(0),
-      -27 to Version(-27),
-    ).forEach { (versionNumber, version) ->
-      "$versionNumber is converted to $version and back" {
-        // expect
-        converter.from(versionNumber) shouldBe version
+  "converts version number to version and back" - {
+    forAll(
+      TestCase(null, null),
+      TestCase(1, Version(1)),
+      TestCase(8, Version(8)),
+      TestCase(0, Version(0)),
+      TestCase(-27, Version(-27)),
+    ) { (versionNumber, version) ->
+      // given
+      val converter = VersionConverter()
 
-        // and
-        converter.to(version) shouldBe versionNumber
-      }
+      // expect
+      converter.from(versionNumber) shouldBe version
+
+      // and
+      converter.to(version) shouldBe versionNumber
     }
   }
 })
