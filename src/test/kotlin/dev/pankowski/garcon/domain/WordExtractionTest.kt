a0a6a21ec96fb3dd0790b0c0e8c370330de3c4bc
Andrzej Pańkowski <github@irratio.nl>
2021-05-02 17:02:20 +0200

    Utilizes Kotest extension for nice test names in word extraction tests

--- src/test/kotlin/dev/pankowski/garcon/domain/WordExtractionTest.kt
+++ src/test/kotlin/dev/pankowski/garcon/domain/WordExtractionTest.kt
@@ -1,25 +1,28 @@
 package dev.pankowski.garcon.domain
 
+import dev.pankowski.garcon.WithTestName
+import dev.pankowski.garcon.forAll
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.core.spec.style.scopes.FreeScope
 import io.kotest.matchers.shouldBe
 import java.util.*
+import java.util.Locale.ENGLISH as EnglishLocale
 
 class WordExtractionTest : FreeSpec({
 
-  data class TestCase(val text: String, val words: List<String>)
+  data class TestCase(val text: String, val words: List<String>) : WithTestName {
+    override fun testName() = "words extracted from '$text' are $words"
+  }
 
   suspend fun FreeScope.verifyExtractions(locale: Locale, vararg testCases: TestCase) =
-    testCases.forEach { (text, words) ->
-      "words extracted from '$text' are $words" {
-        // expect
-        text.extractWords(locale) shouldBe words
-      }
+    forAll(*testCases) { (text, words) ->
+      // expect
+      text.extractWords(locale) shouldBe words
     }
 
-  "should extract words" - {
+  "extracts words" - {
     verifyExtractions(
-      Locale.ENGLISH,
+      EnglishLocale,
       TestCase("This is some text", listOf("This", "is", "some", "text")),
       TestCase(" ", emptyList()),
       TestCase("\n", emptyList()),
@@ -27,18 +30,18 @@ class WordExtractionTest : FreeSpec({
     )
   }
 
-  "should ignore punctuation" - {
+  "ignores punctuation" - {
     verifyExtractions(
-      Locale.ENGLISH,
+      EnglishLocale,
       TestCase("Hello!", listOf("Hello")),
       TestCase("Hello, sir!", listOf("Hello", "sir")),
       TestCase("...what, now?", listOf("what", "now")),
     )
   }
 
-  "should handle language-specific characters" - {
+  "handles language-specific characters" - {
     verifyExtractions(
-      Locale("pl", "PL"),
+      PolishLocale,
       TestCase(" Cześć! ! ", listOf("Cześć")),
       TestCase("Zażółć +gęślą? Jaźń %% ", listOf("Zażółć", "gęślą", "Jaźń")),
     )
