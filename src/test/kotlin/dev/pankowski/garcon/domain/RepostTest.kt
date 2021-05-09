9936d21faa97aab59a58feaa97893c953326d7ca
Andrzej Pańkowski <github@irratio.nl>
2021-05-09 19:34:21 +0200

    Cleans up repost tests

--- src/test/kotlin/dev/pankowski/garcon/domain/RepostTest.kt
+++ src/test/kotlin/dev/pankowski/garcon/domain/RepostTest.kt
@@ -1,43 +1,36 @@
 package dev.pankowski.garcon.domain
 
+import dev.pankowski.garcon.WithTestName
+import dev.pankowski.garcon.forAll
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
 
 class RepostTest : FreeSpec({
 
-  "skipped repost should define its string representation" {
+  "skipped repost defines its string representation" {
     // expect
     Repost.Skip.toString() shouldBe "Skip"
   }
 
-  "skipped repost should have skipped status" {
-    // expect
-    Repost.Skip.status shouldBe RepostStatus.SKIP
-  }
-
-  "pending repost should define its string representation" {
+  "pending repost defines its string representation" {
     // expect
     Repost.Pending.toString() shouldBe "Pending"
   }
 
-  "pending repost should have pending status" {
-    // expect
-    Repost.Pending.status shouldBe RepostStatus.PENDING
-  }
+  "repost has correct status" - {
 
-  "failed repost should have error status" {
-    // given
-    val failed = Repost.Failed(1, now())
+    data class RepostStatusTestCase(val repost: Repost, val status: RepostStatus) : WithTestName {
+      override fun testName() = "${repost::class.simpleName} repost has $status status"
+    }
 
-    // expect
-    failed.status shouldBe RepostStatus.FAILED
-  }
-
-  "successful repost should have success status" {
-    // given
-    val success = Repost.Success(now())
-
-    // expect
-    success.status shouldBe RepostStatus.SUCCESS
+    forAll(
+      RepostStatusTestCase(Repost.Skip, RepostStatus.SKIP),
+      RepostStatusTestCase(Repost.Pending, RepostStatus.PENDING),
+      RepostStatusTestCase(Repost.Failed(1, now()), RepostStatus.FAILED),
+      RepostStatusTestCase(Repost.Success(now()), RepostStatus.SUCCESS),
+    ) { (repost, status) ->
+      // expect
+      repost.status shouldBe status
+    }
   }
 })
