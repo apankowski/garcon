7db57379c0c8d04850635a9fe57465b11486f39a
Andrzej Pańkowski <github@irratio.nl>
2022-02-15 15:35:59 +0100

    Remove redundant annotation from web MVC configurer added via framework configuration class

--- src/main/kotlin/dev/pankowski/garcon/api/ApiConfiguration.kt
+++ src/main/kotlin/dev/pankowski/garcon/api/ApiConfiguration.kt
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory.getLogger
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.stereotype.Component
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
@@ -16,7 +15,6 @@ class ApiConfiguration {
 
   @Bean
   fun webMvcConfigurer(): WebMvcConfigurer =
-    @Component
     object : WebMvcConfigurer {
       override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
         resolvers.add(SlashCommandMethodArgumentResolver())
