28adefe1e1130b6036134c67fe1fa1d57f6880ef
Andrzej Pańkowski <github@irratio.nl>
2022-05-01 19:06:46 +0200

    Upgrades ArchUnit

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,7 @@ dependencyManagement {
     dependency("io.kotest.extensions:kotest-extensions-spring:1.1.1")
     dependency("io.kotest.extensions:kotest-extensions-wiremock:1.0.3")
     dependency("io.mockk:mockk:1.12.3")
-    dependency("com.tngtech.archunit:archunit-junit5:0.22.0")
+    dependency("com.tngtech.archunit:archunit-junit5:0.23.1")
     dependency("com.google.guava:guava:31.0.1-jre")
   }
 }
