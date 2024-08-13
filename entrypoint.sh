95f8fec0b7772ec0dba587618f5d77188256c293
Andrzej Pańkowski <github@irratio.nl>
2024-08-13 20:01:51 +0200

    Improve dockerfile
    Remove entrypoint, use temporary builder container, switch to mainstream Eclipse Temurin JRE distribution

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-exec java $JAVA_OPTS $APP_OPTS \
-  -Djava.security.egd=file:/dev/./urandom \
-  -Dfile.encoding=UTF-8 \
-  -Dorg.jooq.no-logo=true \
-  -Dorg.jooq.no-tips=true \
-  -jar application.jar
-exit $?
