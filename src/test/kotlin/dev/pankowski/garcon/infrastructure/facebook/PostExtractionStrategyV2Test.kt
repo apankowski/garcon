0ff3563efac7415b079885e66c5d93f0db013ed5
Andrzej Pańkowski <github@irratio.nl>
2024-08-14 17:56:58 +0200

    Improve post extraction v2 with slightly different embedded payloads

--- src/test/kotlin/dev/pankowski/garcon/infrastructure/facebook/PostExtractionStrategyV2Test.kt
+++ src/test/kotlin/dev/pankowski/garcon/infrastructure/facebook/PostExtractionStrategyV2Test.kt
@@ -231,9 +231,9 @@ class PostExtractionStrategyV2Test : FreeSpec({
     result shouldContainExactly posts
   }
 
-  "extracts post from a real page (text-only story)" {
+  "extracts post from real page A (text-only story)" {
     // given
-    val document = document(htmlFrom("/lunch/facebook/v2/real-page-text-story.html"))
+    val document = document(htmlFrom("/lunch/facebook/v2/real-page-a-text-story.html"))
 
     // when
     val result = strategy.extractPosts(document)
@@ -326,9 +326,9 @@ class PostExtractionStrategyV2Test : FreeSpec({
     )
   }
 
-  "extracts post from a real page (text story with image)" {
+  "extracts post from real page A (text story with image)" {
     // given
-    val document = document(htmlFrom("/lunch/facebook/v2/real-page-text-story-with-image.html"))
+    val document = document(htmlFrom("/lunch/facebook/v2/real-page-a-text-story-with-image.html"))
 
     // when
     val result = strategy.extractPosts(document)
@@ -342,6 +342,108 @@ class PostExtractionStrategyV2Test : FreeSpec({
         """
         |Nasza nowość sałatka z warzyw korzennych
         |z avocado i hummusem
+        """.trimMargin()
+      ),
+    )
+  }
+
+  "extracts post from real page B (text-only story)" {
+    // given
+    val document = document(htmlFrom("/lunch/facebook/v2/real-page-b-text-story.html"))
+
+    // when
+    val result = strategy.extractPosts(document)
+
+    // then
+    result shouldContainExactly listOf(
+      Post(
+        FacebookPostId("1416281615991685"),
+        URL("https://www.facebook.com/permalink.php?story_fbid=pfbid0dcnDxDnFgyioPGkFVUBAmULwCScvaTSzro4YSANJo7NiV9GwMRJqDisVM3hc8c19l&id=100028295814975"),
+        Instant.ofEpochSecond(1723573975),
+        """
+        |MENU 14 SIERPNIA
+        |ZUPA DNIA
+        |KALAFIOROWA 10 ZŁ
+        |KAPUŚNIAK 10 ZŁ
+        |(PRZY ZAKUPIE DANIA GŁOWNEGO, MAKARONU, SAŁATEK  ZUPA DNIA ZA 6 ZŁ)
+        |
+        |LUNCH
+        |KOTLET MIELONY Z ZIEMNIAKAMI PUREE
+        |I BURACZKAMI  26 ZŁ
+        |TORTILLA Z KURCZAKIEM I WARZYWAMI
+        |Z SOSEM JOGURTOWYM I MIXEM SAŁAT 26 ZŁ
+        |ZIELONE NALEŚNIKI W TEMPURZE
+        |Z WARZYWAMI 26 ZŁ
+        |
+        |ZUPA
+        |CHŁODNIK 25 ZŁ
+        |
+        |PRZEKĄSKI
+        |PANIEROWANY SER HALLOUMI Z OLIWKAMI, PIECZONĄ PAPRYKĄ NA HUMUSSIE
+        |Z CHLEBKIEM PITA 36 ZŁ
+        |DESKA GRECKA: HUMMUS, HITIPITI,
+        |OLIWKI MARYNOWANE,DOLMADAKIA SŁODKO  PIKANTNA, ŚWIEŻE WARZYWA, FETA, PITA 48
+        |TATAR WOŁOWY 43 ZŁ
+        |BRUSCHETTA Z POMIDORAMI
+        |I PESTO BAZYLIOWYM 22 ZŁ
+        |BRUSCHETTA Z KURKAMI
+        |I NATKĄ PIETRUSZKI 26 ZŁ
+        |
+        |SAŁATKI
+        |SAŁATKA CEZAR Z GRILLOWANYM KURCZAKIEM  36 ZŁ
+        |GRILLOWANE WARZYWA: MARCHEWKA, PAPRYKA, CUKINIA Z WOŁOWINĄ I SOSEM SOJOWYM Z SEZAMEM  39 ZŁ
+        |SAŁATKA GRECKA Z GRZANKAMI CZOSNKOWYMI 36 ZŁ
+        |SAŁATKA Z SEREM HALLOUMI NA CARPACCIO Z POMIDORÓW MALINOWYCH Z RUKOLĄ
+        |I DOMOWYM SOSEM PESTO  38 ZŁ
+        |
+        |MAKARONY
+        |CZARNY MAKARON Z KREWETKAMI (5 SZT) SZPINAKIEM, POMIDORKAMI CHERRY W SOSIE MAŚLANO-WINNYM  47 ZŁ
+        |MAKARON Z WOŁOWINĄ, CHORIZO I RUKOLĄ
+        |W SOSIE POMIDOROWYM Z CHILLI  45 ZŁ
+        |MAKARON Z KURCZAKIEM, SZPINAKIEM  ￼
+        |I SZYNKĄ DOJRZEWAJĄCĄ
+        |W KREMOWYM SOSIE  45 ZŁ
+        |MAKARON Z POLĘDWICZKĄ WIEPRZOWĄ
+        |W SOSIE  TRUFLOWYM  43 ZŁ
+        |MAKARON W SOSIE KURKOWYM
+        |Z NATKĄ PIETRUSZKI  41  ZŁ
+        |
+        |RYBY I OWOCE MORZA
+        |ŁOSOŚ PIECZONY W SOSIE CYTRYNOWYM
+        |NA RISOTTO Z ZIELONYM GROSZKIEM  59 ZŁ
+        |MULE W SOSIE MAŚLANO-WINNYM
+        |Z BAGIETKĄ  49 ZŁ
+        |MULE W SOSIE POMIDOROWYM
+        |Z CHILLI Z BAGIETKĄ  49 ZŁ
+        |KREWETKI 10 SZT W SOSIE MAŚLANO – WINNYM  LUB W SOSIE POMIDOROWYM
+        |Z CHILLI I  BAGIETKĄ  59 ZŁ
+        |
+        |BURGERY
+        |BURGER Z ŁOSOSIA Z MUSEM Z AVOCADO, SAŁATĄ RZYMSKĄ,ŚWIEŻYM OGÓRKIEM
+        |I PIKANTNYM SOSEM SZEFA 41 ZŁ
+        |BURGER WEGETARIAŃSKI Z KOTLETEM
+        |Z SOCZEWICY, SAŁATĄ RZYMSKĄ,
+        |POMIDOREM PAPRYKĄ JALAPENO I PESTO  36
+        |BURGER WOŁOWY KLASYCZNY Z RZYMSKĄ SAŁATĄ, POMIDOREM, OGÓRKIEM KONSERWOWYM,  MARYNOWANĄ
+        |CZERWONĄ CEBULĄ I SOSEM SZEFA  38 ZŁ
+        |BURGER WOŁOWY Z JALAPENO, RUKOLĄ, MARYNOWANĄ CEBULĄ, SEREM CHEDDAR
+        |I SOSEM MUSZTARD OWYM  38 ZŁ
+        |DO BURGERA: FRYTKI – 8 ZŁ, MIX SAŁAT – 8 ZŁ
+        |
+        |DANIA GŁÓWNE
+        |ROSTBEF WOŁOWY Z MASŁEM ZIOŁOWYM
+        |Z FRYTKAMI I MIXEM SAŁAT  66 ZŁ
+        |POLĘDWICZKA WIEPRZOWA W SOSIE TRUFLOWYM Z OPIEKANYMI ZIEMNIAKAMI
+        |I BURCZKAMI  47 ZŁ
+        |PIERŚ Z KACZKI W SOSIE TERYAKI
+        |NA BATATACH Z KARMELIZOWANYM
+        |PAK CHOI  48 ZŁ
+        |GRILLOWANA PIERŚ Z KURCZAKA
+        |W SOSIE KURKOWYM Z MŁODYMI ZIEMNIAKAMI I ZIELONĄ SAŁATKĄ 48 ZŁ
+        |
+        |DESER
+        |SERNIK NA ZIMNO 16 ZŁ
+        |CIASTO Z RABARBAREM 16 ZŁ
         """.trimMargin()
       ),
     )
